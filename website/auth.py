345873e0634acb7848603a9c0633d9b03e653569
Jan Stachurski <jan.stachurski@outlook.com>
2024-01-24 11:13:17 +0100

    Zmiana algorytmu hashowania na bcrypt. Dodanie soli. Ograniczenie informacji o odowie logowania

--- website/auth.py
+++ website/auth.py
@@ -1,39 +1,35 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
-from werkzeug.security import generate_password_hash, check_password_hash
-from . import db   ##means from __init__.py import db
+from . import db 
+import bcrypt
 from flask_login import login_user, login_required, logout_user, current_user
-
 
 auth = Blueprint('auth', __name__)
 
+def generate_password_hash(password):
+    bpass = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(bpass, salt)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-
         user = User.query.filter_by(email=email).first()
-        if user:
-            if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
-                login_user(user, remember=True)
-                return redirect(url_for('views.home'))
-            else:
-                flash('Incorrect password, try again.', category='error')
+        if user and bcrypt.checkpw(password.encode('utf-8') ,user.password):
+            flash('Logged in successfully!', category='success')
+            login_user(user, remember=True)
+            return redirect(url_for('views.home'))
         else:
-            flash('Email does not exist.', category='error')
-
+            flash('Invalid email or password.', category='error')
     return render_template("login.html", user=current_user)
-
 
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('auth.login'))
-
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -55,8 +51,7 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                password1, method='sha256'))
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
